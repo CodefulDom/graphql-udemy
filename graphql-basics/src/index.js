c78d6b16ac1a99aef2287980b89f8ab58a86e8de
codefuldom <dominiquehallan@gmail.com>
2018-12-04 23:34:58 -0600

    first commit

---
+++ graphql-basics/src/index.js
@@ -0,0 +1,25 @@
+import { GraphQLServer } from "graphql-yoga";
+
+// Type of Definition
+const typeDefs = `
+	type Query {
+		hello: String!
+	}
+`
+// Resolvers
+const resolvers = {
+	Query: {
+		hello() {
+			return 'This is my first query, bitch!'
+		}
+	}
+}
+
+const server = new GraphQLServer({
+	typeDefs,
+	resolvers
+})
+
+server.start(() => {
+	console.log('The server is up!');
+})
