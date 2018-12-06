ba61558b022728a9d396850a63bdca2c1f5d3914
codefuldom <dominiquehallan@gmail.com>
2018-12-05 23:31:29 -0600

    finished section 2

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,86 +1,187 @@
-import { GraphQLServer } from "graphql-yoga";
+import { GraphQLServer } from 'graphql-yoga'
 
-// Type of Definition
-const typeDefs = `
-	type Query {
-		users(query: String): [User!]!
-		me: User!
-		post: Post!
-	}
+// Scalar types - String, Boolean, Int, Float, ID
 
-	type User {
-		id: ID!
-		name: String!
-		email: String!
-		age: Int
-	}
-
-	type Post {
-		id: ID!
-		title: String!
-		body: String!
-		published: Boolean!
-		rating: Float
-	}
-`
-
-
+// Demo user data
 const users = [{
-	id: '1',
-	name: 'Dominique Hallan',
-	email: 'dominiquehallan@ghost.com',
-	age: 35
+    id: '1',
+    name: 'Andrew',
+    email: 'andrew@example.com',
+    age: 27
 }, {
-	id: '2',
-	name: 'Sarah',
-	email: 'sarah@gmail.com',
-	age: 39
+    id: '2',
+    name: 'Sarah',
+    email: 'sarah@example.com'
 }, {
-	id: '3',
-	name: 'Ralph',
-	email: 'ralphh@gmail.com',
-	age: 39
+    id: '3',
+    name: 'Mike',
+    email: 'mike@example.com'
 }]
 
+const posts = [{
+    id: '10',
+    title: 'GraphQL 101',
+    body: 'This is how to use GraphQL...',
+    published: true,
+    author: '1'
+}, {
+    id: '11',
+    title: 'GraphQL 201',
+    body: 'This is an advanced GraphQL post...',
+    published: false,
+    author: '1'
+}, {
+    id: '12',
+    title: 'Programming Music',
+    body: '',
+    published: false,
+    author: '2'
+}]
 
+const comments = [{
+    id: '102',
+    text: 'This worked well for me. Thanks!',
+    author: '3',
+    post: '10'
+}, {
+    id: '103',
+    text: 'Glad you enjoyed it.',
+    author: '1',
+    post: '10'
+}, {
+    id: '104',
+    text: 'This did no work.',
+    author: '2',
+    post: '11'
+}, {
+    id: '105',
+    text: 'Nevermind. I got it to work.',
+    author: '1',
+    post: '11'
+}]
+
+// Type definitions (schema)
+const typeDefs = `
+    type Query {
+        users(query: String): [User!]!
+        posts(query: String): [Post!]!
+        comments: [Comment!]!
+        me: User!
+        post: Post!
+    }
+
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
+        posts: [Post!]!
+        comments: [Comment!]!
+    }
+
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
+        author: User!
+        comments: [Comment!]!
+    }
+
+    type Comment {
+        id: ID!
+        text: String!
+        author: User!
+        post: Post!
+    }
+`
 
 // Resolvers
 const resolvers = {
-	Query: {
-		users(parent, args, ctx, info) {
-			if (!args.query) {
-				return users
-			}
-			return users.filter((user) => {
-				return user.name.toLowerCase().includes(args.query.toLowerCase())
-			})
-		},
-		me() {
-			return {
-				id: 123098,
-				name: 'Mike Jones',
-				email: 'mikejones@gmail.com',
-				age: 23
-			}
-		},
-		post() {
-			return {
-				id: 145667,
-				title: 'The Developer Life',
-				body: 'There are many things that I can tell you about what it is like going from McDonald\'s overnight employee, but I can show you much more effectivley.',
-				published: true,
-				rating: 5.0
-			}
-		}
-	}
-}
+    Query: {
+        users(parent, args, ctx, info) {
+            if (!args.query) {
+                return users
+            }
 
+            return users.filter((user) => {
+                return user.name.toLowerCase().includes(args.query.toLowerCase())
+            })
+        },
+        posts(parent, args, ctx, info) {
+            if (!args.query) {
+                return posts
+            }
+
+            return posts.filter((post) => {
+                const isTitleMatch = post.title.toLowerCase().includes(args.query.toLowerCase())
+                const isBodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase())
+                return isTitleMatch || isBodyMatch
+            })
+        },
+        comments(parent, args, ctx, info) {
+            return comments
+        },
+        me() {
+            return {
+                id: '123098',
+                name: 'Mike',
+                email: 'mike@example.com'
+            }
+        },
+        post() {
+            return {
+                id: '092',
+                title: 'GraphQL 101',
+                body: '',
+                published: false
+            }
+        }
+    },
+    Post: {
+        author(parent, args, ctx, info) {
+            return users.find((user) => {
+                return user.id === parent.author
+            })
+        },
+        comments(parent, args, ctx, info) {
+            return comments.filter((comment) => {
+                return comment.post === parent.id
+            })
+        }
+    },
+    Comment: {
+        author(parent, args, ctx, info) {
+            return users.find((user) => {
+                return user.id === parent.author
+            })
+        },
+        post(parent, args, ctx, info) {
+            return posts.find((post) => {
+                return post.id === parent.post
+            })
+        }
+    },
+    User: {
+        posts(parent, args, ctx, info) {
+            return posts.filter((post) => {
+                return post.author === parent.id
+            })
+        },
+        comments(parent, args, ctx, info) {
+            return comments.filter((comment) => {
+                return comment.author === parent.id
+            })
+        }
+    }
+}
 const server = new GraphQLServer({
 	typeDefs,
 	resolvers
 })
 
 server.start(() => {
-	console.log('The server is up Dom!');
+	console.log('server is up Dom!');
 })
 
+process.on('SIGINT', () => { console.log("Bye bye Dom! Server is closed!!"); process.exit(); });
